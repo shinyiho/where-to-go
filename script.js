8adfb0baf63bfe6dd25487d48e57dfa6c1609103
Glitch (hello-webpage) <none>
2020-11-13 15:04:58 +0000

    🐿💍 Checkpoint ./index.html:11276549/205 ./script.js:11276549/78

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 // prints "hi" in the browser's dev tools console
 console.log("hi");
 
-
+let 
 
 let cloth = <img src="https://img.icons8.com/emoji/48/000000/womans-clothes.png"/>
+    <img src="https://img.icons8.com/plasticine/100/000000/sweater.png"/>
