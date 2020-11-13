95fa9193eb81f5775146f6494493389e1efdd9f8
Glitch (hello-webpage) <none>
2020-11-13 15:29:29 +0000

    🙍🛣 Checkpoint ./index.html:11276549/405 ./script.js:11276549/1937 ./style.css:11276549/15

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 
-let clothChoices = ["https://img.icons8.com/emoji/48/000000/womans-clothes.png","https://img.icons8.com/plasticine/100/000000/sweater.png"]
+let clothChoices = ["https://img.icons8.com/emoji/300/000000/womans-clothes.png","https://img.icons8.com/plasticine/300/000000/sweater.png"]
 let clothChoicesPointer = 0
-let cloth = document.getelementbyID("clothImg")
-
-
+let cloth = document.getElementById("clothImg")
+let pantChoices = ["https://img.icons8.com/emoji/300/000000/womans-clothes.png","https://img.icons8.com/plasticine/300/000000/sweater.png"]
+let pantChoicesPointer = 0
+let pant = document.getElementById("pantImg")
       
       
-let clothLeft = () =>{
+function clothLeft(){
+  console.log(cloth)
   clothChoicesPointer = clothChoicesPointer-1 < 0? clothChoices.length - 1:clothChoicesPointer - 1
-  image.src = clothChoices[clothChoicesPointer]
+  cloth.src = clothChoices[clothChoicesPointer]
   
 }
-let clothRight = () =>{
+// }
+
+function clothRight(){
+  console.log(cloth)
   clothChoicesPointer = clothChoicesPointer+1 > clothChoices.length - 1? 0 :clothChoicesPointer + 1
-  image.src = clothChoices[clothChoicesPointer]
+  cloth.src = clothChoices[clothChoicesPointer]
+  
+}
+
+function pantLeft(){
+  console.log(pant)
+  pantChoicesPointer = pantChoicesPointer-1 < 0? pantChoices.length - 1:pantChoicesPointer - 1
+  pant.src = pantChoices[pantChoicesPointer]
+  
+}
+// }
+
+function pantRight(){
+  console.log(pant)
+  pantChoicesPointer = pantChoicesPointer+1 > pantChoices.length - 1? 0 :pantChoicesPointer + 1
+  pant.src = pantChoices[pantChoicesPointer]
   
 }
