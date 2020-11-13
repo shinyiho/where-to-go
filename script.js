8e5bab1a661edd73d115996e45f103c809b5eb95
Glitch (hello-webpage) <none>
2020-11-13 20:30:57 +0000

    ⛸🎬 Checkpoint ./script.js:11276549/3338 ./index.html:11276549/187 ./style.css:11276549/44

--- script.js
+++ script.js
@@ -5,13 +5,33 @@ let clothChoices = ["https://img.icons8.com/emoji/300/000000/womans-clothes.png"
                    "https://img.icons8.com/color/300/000000/worn-out-clothes.png"]
 let clothChoicesPointer = 0
 let cloth = document.getElementById("clothImg")
+
 let pantChoices = ["https://img.icons8.com/office/300/000000/jeans.png",
                   "https://img.icons8.com/officel/300/000000/jeans.png",
                    "https://img.icons8.com/color/300/000000/womens-pants--v2.png"]
 let pantChoicesPointer = 0
 let pant = document.getElementById("pantImg")
+
+let eyeChoices = ["https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Feye1.png?v=1605294886281",
+                  "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Feye3.png?v=1605294888696",
+                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Feye7.png?v=1605294889324",
+                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Feye2.png?v=1605294889485"]
+let eyeChoicesPointer = 0
 let eye = document.getElementById("eye")
+
+
+let noseChoices = ["https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fnose1.png?v=1605294888627",
+                  "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fnose2.png?v=1605294888848",
+                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fnose3.png?v=1605294889417"]
+let noseChoicesPointer = 0
 let nose = document.getElementById("nose")
+
+
+let lipChoices = ["https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Flip.png?v=1605294888448",
+                  "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Feye3.png?v=1605294888696",
+                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Feye7.png?v=1605294889324",
+                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Feye2.png?v=1605294889485"]
+let lipChoicesPointer = 0
 let lip = document.getElementById("lip")
 
       
@@ -45,5 +65,17 @@ function pantRight(){
 }
 
 function changeEye(){
-  eye.src = pantChoices[pantChoicesPointer]
+   console.log("changeeye")
+  eyeChoicesPointer = eyeChoicesPointer+1 > eyeChoices.length - 1? 0 :eyeChoicesPointer + 1
+  eye.src = eyeChoices[eyeChoicesPointer]
+}
+function changeNose(){
+   console.log("nose")
+  noseChoicesPointer = noseChoicesPointer+1 > noseChoices.length - 1? 0 :noseChoicesPointer + 1
+  nose.src = noseChoices[noseChoicesPointer]
+}
+function changeLip(){
+   console.log("lip")
+  lipChoicesPointer = lipChoicesPointer+1 > lipChoices.length - 1? 0 :lipChoicesPointer + 1
+  lip.src = lipChoices[lipChoicesPointer]
 }
