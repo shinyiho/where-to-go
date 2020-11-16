ecc86a41c49f90bb69d89e0ef7a30aa6b7d3e6bb
Glitch (hello-webpage) <none>
2020-11-16 22:06:17 +0000

    ⚽️🐗 Checkpoint ./script.js:11276549/20474 ./style.css:11276549/4057 ./index.html:11276549/5307

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
+// let clothChoices = [
+//   // "https://img.icons8.com/emoji/200/000000/womans-clothes.png",
+//   //  "https://img.icons8.com/plasticine/200/000000/sweater.png",
+//   //  "https://img.icons8.com/plasticine/200/000000/clothes.png",
+//   // "https://img.icons8.com/color/200/000000/worn-out-clothes.png",
+//   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2F%E5%84%B2%E5%AD%983.png?v=1605364493122",
+//   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fcloth.png?v=1605365166273",
+//   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2F%E5%86%B0.png?v=1605366089790",
+//   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2F%E5%88%80%E5%93%A5.png?v=1605366573381",
+//   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fron1.png?v=1605371163455",
+//   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fgrass.png?v=1605373256359",
+//   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fsnake.png?v=1605373256330",
+//   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fpurple.png?v=1605373256478",
+//   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fcloud.png?v=1605373256573"
+// ];
+// let clothChoicesPointer = 0;
+let cloth = document.getElementById("clothImg");
 
-let clothChoices = [
-                    // "https://img.icons8.com/emoji/200/000000/womans-clothes.png",
-                   //  "https://img.icons8.com/plasticine/200/000000/sweater.png",
-                   //  "https://img.icons8.com/plasticine/200/000000/clothes.png",
-                   // "https://img.icons8.com/color/200/000000/worn-out-clothes.png",
-                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2F%E5%84%B2%E5%AD%983.png?v=1605364493122",
-                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fcloth.png?v=1605365166273",
-                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2F%E5%86%B0.png?v=1605366089790",
-                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2F%E5%88%80%E5%93%A5.png?v=1605366573381",
-                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fron1.png?v=1605371163455",
-                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fgrass.png?v=1605373256359",
-                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fsnake.png?v=1605373256330",
-                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fpurple.png?v=1605373256478",
-                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fcloud.png?v=1605373256573"]
-let clothChoicesPointer = 0
-let cloth = document.getElementById("clothImg")
-
-let pantChoices = ["https://img.icons8.com/office/200/000000/jeans.png",
-                  "https://img.icons8.com/officel/200/000000/jeans.png",
-                   "https://img.icons8.com/color/200/000000/womens-pants--v2.png",
-                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2F%E8%97%8D%E8%A4%B2.png?v=1605367817924",
-                  "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fcircle.png?v=1605371163455",
-                  "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fron.png?v=1605371163455",
-                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Ffish.png?v=1605371163522",
-                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fpumkin.png?v=1605371163608"]
-let pantChoicesPointer = 0
-let pant = document.getElementById("pantImg")
+// let pantChoices = [
+//   "https://img.icons8.com/office/200/000000/jeans.png",
+//   "https://img.icons8.com/officel/200/000000/jeans.png",
+//   "https://img.icons8.com/color/200/000000/womens-pants--v2.png",
+//   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2F%E8%97%8D%E8%A4%B2.png?v=1605367817924",
+//   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fcircle.png?v=1605371163455",
+//   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fron.png?v=1605371163455",
+//   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Ffish.png?v=1605371163522",
+//   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fpumkin.png?v=1605371163608"
+// ];
+// let pantChoicesPointer = 0;
+let pant = document.getElementById("pantImg");
 
 // let eyeChoices = ["https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Feye1.png?v=1605294886281",
 //                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Feye3.png?v=1605294888696",
 //                    "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Feye7.png?v=1605294889324",
 //                    "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Feye2.png?v=1605294889485"]
 // let eyeChoicesPointer = 0
-let eye = document.getElementById("eye")
-
+let eye = document.getElementById("eye");
 
 // let noseChoices = ["https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fnose2.png?v=1605294888848",
 //                    "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fnose1.png?v=1605294888627",
 //                    "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fnose3.png?v=1605294889417"]
 // let noseChoicesPointer = 0
-let nose = document.getElementById("nose")
-
+let nose = document.getElementById("nose");
 
 // let lipChoices = ["https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Flip2.png?v=1605294888502",
 //                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Flip.png?v=1605294888448"]
 // let lipChoicesPointer = 0
-let mouth = document.getElementById("mouth")
+let mouth = document.getElementById("mouth");
 
 // let faceChoices = ["https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fface.png?v=1605295108978",
 //                    "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fface3.png?v=1605302847791",
@@ -57,8 +57,7 @@ let mouth = document.getElementById("mouth")
 //                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fface4.png?v=1605302848002",
 //                   "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2Fface9.png?v=1605302848280"]
 // let faceChoicesPointer = 0
-let face = document.getElementById("face")
-
+let face = document.getElementById("face");
 
 // let viewChoices = ["https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2FIMG_3914_p.png?v=1605376319243",
 //                    "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2FIMG_3915_p.png?v=1605376319324",
@@ -77,35 +76,42 @@ let face = document.getElementById("face")
 //                    "https://cdn.glitch.com/1db309ec-6a57-4600-8474-072f16a1ba36%2FIMG_6042_p.png?v=1605381716181"
 //                    ]
 // let viewChoicesPointer = 0
-let background = document.getElementById("background")
-      
-      
-function clothLeft(){
-  console.log(cloth)
-  clothChoicesPointer = clothChoicesPointer-1 < 0? clothChoices.length - 1:clothChoicesPointer - 1
-  cloth.src = clothChoices[clothChoicesPointer]
-  
+let background = document.getElementById("background");
+
+function clothLeft() {
+  console.log(cloth);
+  clothChoicesPointer =
+    clothChoicesPointer - 1 < 0
+      ? clothChoices.length - 1
+      : clothChoicesPointer - 1;
+  cloth.src = clothChoices[clothChoicesPointer];
 }
 
-function clothRight(){
-  console.log(cloth)
-  clothChoicesPointer = clothChoicesPointer+1 > clothChoices.length - 1? 0 :clothChoicesPointer + 1
-  cloth.src = clothChoices[clothChoicesPointer]
-  
+function clothRight() {
+  console.log(cloth);
+  clothChoicesPointer =
+    clothChoicesPointer + 1 > clothChoices.length - 1
+      ? 0
+      : clothChoicesPointer + 1;
+  cloth.src = clothChoices[clothChoicesPointer];
 }
 
-function pantLeft(){
-  console.log(pant)
-  pantChoicesPointer = pantChoicesPointer-1 < 0? pantChoices.length - 1:pantChoicesPointer - 1
-  pant.src = pantChoices[pantChoicesPointer]
-  
+function pantLeft() {
+  console.log(pant);
+  pantChoicesPointer =
+    pantChoicesPointer - 1 < 0
+      ? pantChoices.length - 1
+      : pantChoicesPointer - 1;
+  pant.src = pantChoices[pantChoicesPointer];
 }
 
-function pantRight(){
-  console.log(pant)
-  pantChoicesPointer = pantChoicesPointer+1 > pantChoices.length - 1? 0 :pantChoicesPointer + 1
-  pant.src = pantChoices[pantChoicesPointer]
-  
+function pantRight() {
+  console.log(pant);
+  pantChoicesPointer =
+    pantChoicesPointer + 1 > pantChoices.length - 1
+      ? 0
+      : pantChoicesPointer + 1;
+  pant.src = pantChoices[pantChoicesPointer];
 }
 
 // function changeEye(){
@@ -118,12 +124,12 @@ function pantRight(){
 //   noseChoicesPointer = noseChoicesPointer+1 > noseChoices.length - 1? 0 :noseChoicesPointer + 1
 //   nose.src = noseChoices[noseChoicesPointer]
 // }
-function changeLip(){
-   console.log("lip")
-  lipChoicesPointer = lipChoicesPointer+1 > lipChoices.length - 1? 0 :lipChoicesPointer + 1
-  lip.src = lipChoices[lipChoicesPointer]
+function changeLip() {
+  console.log("lip");
+  lipChoicesPointer =
+    lipChoicesPointer + 1 > lipChoices.length - 1 ? 0 : lipChoicesPointer + 1;
+  lip.src = lipChoices[lipChoicesPointer];
 }
-
 
 // function changeFace(){
 //    console.log("face")
@@ -131,43 +137,68 @@ function changeLip(){
 //   face.src = faceChoices[faceChoicesPointer]
 // }
 
-
-
-
-function changeView(){
-   console.log(document.width)
-  viewChoicesPointer = viewChoicesPointer+1 > viewChoices.length - 1? 0 :viewChoicesPointer + 1
-  view.style = `background:url(${viewChoices[viewChoicesPointer]});background-size:920px 710px;`
+function changeView() {
+  console.log(document.width);
+  viewChoicesPointer =
+    viewChoicesPointer + 1 > viewChoices.length - 1
+      ? 0
+      : viewChoicesPointer + 1;
+  view.style = `background:url(${viewChoices[viewChoicesPointer]});background-size:920px 710px;`;
   //這樣會覆蓋整個style想想看如何加上去而不是覆蓋1344px 1008px
 }
 
+document.querySelectorAll(".accessoriesOption").forEach(item => {
+  item.addEventListener("click", toggleAccessories);
+});
 
-document.querySelectorAll('.accessoriesOption').forEach(item => { item.addEventListener('click', toggleAccessories);})
-
-function toggleAccessories(e){
-   face.src = e.target.getAttribute('imgurl');
+function toggleAccessories(e) {
+  face.src = e.target.getAttribute("imgurl");
 }
 
-document.querySelectorAll('.eyesOption').forEach(item => { item.addEventListener('click', toggleEyes);})
+document.querySelectorAll(".eyesOption").forEach(item => {
+  item.addEventListener("click", toggleEyes);
+});
 
-function toggleEyes(e){
-   eye.src = e.target.getAttribute('imgurl');
+function toggleEyes(e) {
+  eye.src = e.target.getAttribute("imgurl");
 }
 
-document.querySelectorAll('.nosesOption').forEach(item => { item.addEventListener('click', toggleNoses);})
+document.querySelectorAll(".nosesOption").forEach(item => {
+  item.addEventListener("click", toggleNoses);
+});
 
-function toggleNoses(e){
-   nose.src = e.target.getAttribute('imgurl');
+function toggleNoses(e) {
+  nose.src = e.target.getAttribute("imgurl");
 }
 
-document.querySelectorAll('.mouthsOption').forEach(item => { item.addEventListener('click', toggleMouths);})
+document.querySelectorAll(".mouthsOption").forEach(item => {
+  item.addEventListener("click", toggleMouths);
+});
 
-function toggleMouths(e){
-   mouth.src = e.target.getAttribute('imgurl');
+function toggleMouths(e) {
+  mouth.src = e.target.getAttribute("imgurl");
 }
 
-document.querySelectorAll('.backgroundsOption').forEach(item => { item.addEventListener('click', toggleBackgrounds);})
+document.querySelectorAll(".backgroundsOption").forEach(item => {
+  item.addEventListener("click", toggleBackgrounds);
+});
 
-function toggleBackgrounds(e){
-   background.src = `background:url(${e.target.getAttribute('imgurl')});
+function toggleBackgrounds(e) {
+  background.src = e.target.getAttribute("imgurl");
+}
+
+document.querySelectorAll(".topsOption").forEach(item => {
+  item.addEventListener("click", toggleTops);
+});
+
+function toggleTops(e) {
+  cloth.src = e.target.getAttribute("imgurl");
+}
+
+document.querySelectorAll(".bottomsOption").forEach(item => {
+  item.addEventListener("click", togglebottoms);
+});
+
+function togglebottoms(e) {
+  pant.src = e.target.getAttribute("imgurl");
 }
